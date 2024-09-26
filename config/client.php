731fe22a93ba7864c4363efae9b9a2f34a161e6e
Marcel Stadelmann <m@marceli.to>
2024-09-26 16:00:45 +0200

    wip

---
+++ config/client.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'base_url' => env('APP_URL'),
+  'meta_description' => 'An der Aumattstrasse 70/72 wird per Frühsommer 2025 eine neue, lebendige und generationenübergreifende Wohnüberbauung realisiert.'
+];
