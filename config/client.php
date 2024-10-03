d16852a5f2854a67d923133369468454e115ff5c
Marcel Stadelmann <m@marceli.to>
2024-10-03 14:00:39 +0200

    wip

--- config/client.php
+++ config/client.php
@@ -1,5 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
-  'meta_description' => 'An der Aumattstrasse 70/72 wird per Frühsommer 2025 eine neue, lebendige und generationenübergreifende Wohnüberbauung realisiert.'
+  'meta_description' => 'An der Aumattstrasse 70/72/74 wird per Frühsommer 2025 eine neue und lebendige Wohnüberbauung realisiert. Es entstehen moderne 4.5- und 5.5-Zimmer-Wohnungen, ideal für Paare oder Familien sowie altersgerechte 2.5- und 3.5-Zimmer-Wohnungen, bei welchen auch Zusatzleistungen (zum Beispiel Betreuung oder Service-Dienstleistungen) von dem benachbarten Seniorenzentrum Aumatt bezogen werden können.'
 ];
